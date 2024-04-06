f641512f5d19bdbda480bff348a62ee6081c5f0d
Pedro Maurício de Almeida Chagas <pedromauricio360@gmail.com>
2024-04-06 13:10:07 -0300

    Subindo exercicios

---
+++ exercicio-8/script.js
@@ -0,0 +1,18 @@
+function calcularMedia() {
+    let nota1 = parseFloat(document.getElementById("primeiraNota").value);
+    let nota2 = parseFloat(document.getElementById("segundaNota").value);
+    let nota3 = parseFloat(document.getElementById("terceiraNota").value);
+    let nota4 = parseFloat(document.getElementById("quartaNota").value);
+    let calculo = (nota1 + nota2 + nota3 + nota4) / 4;
+    
+    document.getElementById("resultado").innerHTML = `A média do aluno é de ${parseInt(calculo)}.`;
+    
+    if (calculo==10) { 
+        document.getElementById("resultadoPlus").innerHTML = "Aprovado com Distinção.";
+    } else if (calculo>=7) {
+        document.getElementById("resultadoPlus").innerHTML = "Aprovado.";
+    }
+    else {
+        document.getElementById("resultadoPlus").innerHTML = " Reprovado.";
+    }
+}
