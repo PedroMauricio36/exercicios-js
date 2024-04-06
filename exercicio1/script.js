b5771d99be491d2301119b8190a43db278226e2a
Pedro Mauricio De Almeida Chagas <114452732+PedroMauricio36@users.noreply.github.com>
2024-04-06 08:16:06 -0300

    Add files via upload

---
+++ exercicio1/script.js
@@ -0,0 +1,10 @@
+
+
+function calcularAnoNascimento() {
+    let anoAtual = parseInt(document.getElementById("anoAtual").value);
+    let idade = parseInt(document.getElementById("idade").value);
+    
+    let anoNascimento = anoAtual - idade;
+    
+    document.getElementById("resultado").innerHTML = "Você nasceu em " + anoNascimento + ".";
+}
